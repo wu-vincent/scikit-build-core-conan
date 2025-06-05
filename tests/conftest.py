662fe5df01986a718fcacda526a2028f91fc5d1d
Vincent <magicdroidx@gmail.com>
2025-06-05 13:29:41 +0100

    feat: add tests

---
+++ tests/conftest.py
@@ -0,0 +1,410 @@
+from __future__ import annotations
+
+import contextlib
+import dataclasses
+import importlib.util
+import os
+import shutil
+import subprocess
+import sys
+import sysconfig
+from importlib import metadata
+from pathlib import Path
+from typing import Any, Literal, overload
+
+import virtualenv as _virtualenv
+
+if sys.version_info < (3, 11):
+    import tomli as tomllib
+else:
+    import tomllib
+
+import pytest
+from packaging.requirements import Requirement
+from packaging.version import Version
+
+DIR = Path(__file__).parent.resolve()
+BASE = DIR.parent
+
+VIRTUALENV_VERSION = Version(metadata.version("virtualenv"))
+
+
+@pytest.fixture(scope="session")
+def pep518_wheelhouse(tmp_path_factory: pytest.TempPathFactory) -> Path:
+    wheelhouse = tmp_path_factory.mktemp("wheelhouse")
+
+    subprocess.run(
+        [
+            sys.executable,
+            "-m",
+            "pip",
+            "wheel",
+            "--wheel-dir",
+            str(wheelhouse),
+            f"{BASE}",
+        ],
+        check=True,
+    )
+    packages = [
+        "build",
+        "cython",
+        "hatchling",
+        "pip",
+        "pybind11",
+        "setuptools",
+        "virtualenv",
+        "wheel",
+    ]
+
+    if importlib.util.find_spec("cmake") is not None:
+        packages.append("cmake")
+
+    if importlib.util.find_spec("ninja") is not None:
+        packages.append("ninja")
+
+    subprocess.run(
+        [
+            sys.executable,
+            "-m",
+            "pip",
+            "download",
+            "-q",
+            "-d",
+            str(wheelhouse),
+            *packages,
+        ],
+        check=True,
+    )
+    return wheelhouse
+
+
+class VEnv:
+    def __init__(self, env_dir: Path, *, wheelhouse: Path | None = None) -> None:
+        cmd = [str(env_dir), "--no-setuptools", "--activators", ""]
+        if Version("20.31.0") > VIRTUALENV_VERSION:
+            cmd.append("--no-wheel")
+        result = _virtualenv.cli_run(cmd, setup_logging=False)
+        self.wheelhouse = wheelhouse
+        self.executable = Path(result.creator.exe)
+        self.env_dir = env_dir.resolve()
+        self.platlib = Path(
+            self.execute("import sysconfig; print(sysconfig.get_path('platlib'))")
+        )
+        self.purelib = Path(
+            self.execute("import sysconfig; print(sysconfig.get_path('purelib'))")
+        )
+
+    @overload
+    def run(self, *args: str, capture: Literal[True]) -> str:
+        ...
+
+    @overload
+    def run(self, *args: str, capture: Literal[False] = ...) -> None:
+        ...
+
+    def run(self, *args: str, capture: bool = False) -> str | None:
+        __tracebackhide__ = True
+        env = os.environ.copy()
+        paths = {str(self.executable.parent)}
+        env["PATH"] = os.pathsep.join([*paths, env["PATH"]])
+        env["VIRTUAL_ENV"] = str(self.env_dir)
+        env["PIP_DISABLE_PIP_VERSION_CHECK"] = "ON"
+        if self.wheelhouse is not None:
+            env["PIP_NO_INDEX"] = "ON"
+            env["PIP_FIND_LINKS"] = str(self.wheelhouse)
+
+        str_args = [os.fspath(a) for a in args]
+
+        # Windows does not make a python shortcut in venv
+        if str_args[0] in {"python", "python3"}:
+            str_args[0] = str(self.executable)
+
+        if capture:
+            result = subprocess.run(
+                str_args,
+                check=False,
+                capture_output=True,
+                text=True,
+                env=env,
+            )
+            if result.returncode != 0:
+                print(result.stdout, file=sys.stdout)
+                print(result.stderr, file=sys.stderr)
+                print("FAILED RUN:", *str_args, file=sys.stderr)
+                raise SystemExit(result.returncode)
+            return result.stdout.strip()
+
+        result_bytes = subprocess.run(
+            str_args,
+            check=False,
+            env=env,
+        )
+        if result_bytes.returncode != 0:
+            print("FAILED RUN:", *str_args, file=sys.stderr)
+            raise SystemExit(result_bytes.returncode)
+        return None
+
+    def execute(self, command: str) -> str:
+        return self.run(str(self.executable), "-c", command, capture=True)
+
+    def module(self, *args: str) -> None:
+        return self.run(str(self.executable), "-m", *args)
+
+    def install(self, *args: str, isolated: bool = True) -> None:
+        isolated_flags = "" if isolated else ["--no-build-isolation"]
+        self.module("pip", "install", *isolated_flags, *args)
+
+
+@pytest.fixture
+def isolated(tmp_path: Path, pep518_wheelhouse: Path) -> VEnv:
+    path = tmp_path / "venv"
+    return VEnv(path, wheelhouse=pep518_wheelhouse)
+
+
+@pytest.fixture
+def virtualenv(tmp_path: Path) -> VEnv:
+    path = tmp_path / "venv"
+    return VEnv(path)
+
+
+@dataclasses.dataclass(frozen=True)
+class PackageInfo:
+    name: str
+    sdist_hash38: str | None = None
+    sdist_hash39: str | None = None
+    sdist_dated_hash39: str | None = None
+    sdist_dated_hash38: str | None = None
+
+    @property
+    def sdist_hash(self) -> str | None:
+        return self.sdist_hash38 if sys.version_info < (3, 9) else self.sdist_hash39
+
+    @property
+    def sdist_dated_hash(self) -> str | None:
+        return (
+            self.sdist_dated_hash38
+            if sys.version_info < (3, 9)
+            else self.sdist_dated_hash39
+        )
+
+    @property
+    def source_date_epoch(self) -> str:
+        return "12345"
+
+
+def process_package(
+        package: PackageInfo, tmp_path: Path, monkeypatch: pytest.MonkeyPatch
+) -> None:
+    package_dir = tmp_path / "pkg"
+    shutil.copytree(DIR / "packages" / package.name, package_dir)
+    monkeypatch.chdir(package_dir)
+
+
+@pytest.fixture
+def package_simple_pyproject_ext(
+        tmp_path: Path, monkeypatch: pytest.MonkeyPatch
+) -> PackageInfo:
+    package = PackageInfo(
+        "simple_pyproject_ext",
+        "71b4e95854ef8d04886758d24d18fe55ebe63648310acf58c7423387cca73508",
+        "ed930179fbf5adc2e71a64a6f9686c61fdcce477c85bc94dd51598641be886a7",
+        "0178462b64b4eb9c41ae70eb413a9cc111c340e431b240af1b218fe81b0c2ecb",
+        "de79895a9d5c2112257715214ab419d3635e841716655e8a55390e5d52445819",
+    )
+    process_package(package, tmp_path, monkeypatch)
+    return package
+
+
+@pytest.fixture
+def package_simple_pyproject_script_with_flags(
+        tmp_path: Path, monkeypatch: pytest.MonkeyPatch
+) -> PackageInfo:
+    package = PackageInfo(
+        "simple_pyproject_script_with_flags",
+    )
+    process_package(package, tmp_path, monkeypatch)
+    return package
+
+
+@pytest.fixture
+def package_simple_pyproject_source_dir(
+        tmp_path: Path, monkeypatch: pytest.MonkeyPatch
+) -> PackageInfo:
+    package = PackageInfo(
+        "simple_pyproject_source_dir",
+    )
+    process_package(package, tmp_path, monkeypatch)
+    return package
+
+
+@pytest.fixture
+def package_simple_setuptools_ext(
+        tmp_path: Path, monkeypatch: pytest.MonkeyPatch
+) -> PackageInfo:
+    package = PackageInfo("simple_setuptools_ext")
+    process_package(package, tmp_path, monkeypatch)
+    return package
+
+
+@pytest.fixture
+def package_toml_setuptools_ext(
+        tmp_path: Path, monkeypatch: pytest.MonkeyPatch
+) -> PackageInfo:
+    package = PackageInfo("toml_setuptools_ext")
+    process_package(package, tmp_path, monkeypatch)
+    return package
+
+
+@pytest.fixture
+def package_mixed_setuptools(
+        tmp_path: Path, monkeypatch: pytest.MonkeyPatch
+) -> PackageInfo:
+    package = PackageInfo("mixed_setuptools")
+    process_package(package, tmp_path, monkeypatch)
+    return package
+
+
+@pytest.fixture
+def package_filepath_pure(
+        tmp_path: Path, monkeypatch: pytest.MonkeyPatch
+) -> PackageInfo:
+    package = PackageInfo("filepath_pure")
+    process_package(package, tmp_path, monkeypatch)
+    return package
+
+
+@pytest.fixture
+def package_dynamic_metadata(
+        tmp_path: Path, monkeypatch: pytest.MonkeyPatch
+) -> PackageInfo:
+    package = PackageInfo("dynamic_metadata")
+    process_package(package, tmp_path, monkeypatch)
+    return package
+
+
+@pytest.fixture
+def package_hatchling(tmp_path: Path, monkeypatch: pytest.MonkeyPatch) -> PackageInfo:
+    package = PackageInfo("hatchling")
+    process_package(package, tmp_path, monkeypatch)
+    return package
+
+
+@pytest.fixture
+def package_simplest_c(tmp_path: Path, monkeypatch: pytest.MonkeyPatch) -> PackageInfo:
+    package = PackageInfo(
+        "simplest_c",
+    )
+    process_package(package, tmp_path, monkeypatch)
+    return package
+
+
+@pytest.fixture
+def navigate_editable(tmp_path: Path, monkeypatch: pytest.MonkeyPatch) -> PackageInfo:
+    package = PackageInfo(
+        "navigate_editable",
+    )
+    process_package(package, tmp_path, monkeypatch)
+    return package
+
+
+@pytest.fixture
+def broken_fallback(tmp_path: Path, monkeypatch: pytest.MonkeyPatch) -> PackageInfo:
+    package = PackageInfo(
+        "broken_fallback",
+    )
+    process_package(package, tmp_path, monkeypatch)
+    return package
+
+
+@pytest.fixture
+def package_sdist_config(
+        tmp_path: Path, monkeypatch: pytest.MonkeyPatch
+) -> PackageInfo:
+    package = PackageInfo(
+        "sdist_config",
+    )
+    process_package(package, tmp_path, monkeypatch)
+    return package
+
+
+@pytest.fixture
+def package_simple_purelib_package(
+        tmp_path: Path, monkeypatch: pytest.MonkeyPatch
+) -> PackageInfo:
+    package = PackageInfo(
+        "simple_purelib_package",
+    )
+    process_package(package, tmp_path, monkeypatch)
+    return package
+
+
+@pytest.fixture
+def package_pep639_pure(tmp_path: Path, monkeypatch: pytest.MonkeyPatch) -> PackageInfo:
+    package = PackageInfo(
+        "pep639_pure",
+    )
+    process_package(package, tmp_path, monkeypatch)
+    return package
+
+
+def which_mock(name: str) -> str | None:
+    if name in {"ninja", "ninja-build", "cmake3", "samu", "gmake", "make"}:
+        return None
+    if name == "cmake":
+        return "cmake/path"
+    return None
+
+
+@pytest.fixture
+def protect_get_requires(fp, monkeypatch):
+    """
+    Protect get_requires from actually calling anything variable during tests.
+    """
+    # This needs to be passed due to packaging.tags 22 extra checks if macos 10.16 is reported
+    fp.pass_command([sys.executable, fp.any()])
+    monkeypatch.setattr(shutil, "which", which_mock)
+    monkeypatch.delenv("CMAKE_GENERATOR", raising=False)
+
+    orig_find_spec = importlib.util.find_spec
+
+    def find_spec(name: str, package: str | None = None) -> Any:
+        if name in {"cmake", "ninja"}:
+            return None
+        return orig_find_spec(name, package)
+
+    monkeypatch.setattr(importlib.util, "find_spec", find_spec)
+
+
+def pytest_collection_modifyitems(items: list[pytest.Item]) -> None:
+    for item in items:
+        # Ensure all tests using virtualenv are marked as such
+        if "virtualenv" in getattr(item, "fixturenames", ()):
+            item.add_marker(pytest.mark.virtualenv)
+        if "isolated" in getattr(item, "fixturenames", ()):
+            item.add_marker(pytest.mark.virtualenv)
+            item.add_marker(pytest.mark.isolated)
+            item.add_marker(pytest.mark.network)
+
+
+def pytest_report_header() -> str:
+    with BASE.joinpath("pyproject.toml").open("rb") as f:
+        pyproject = tomllib.load(f)
+    project = pyproject.get("project", {})
+
+    pkgs = project.get("dependencies", [])
+    pkgs += [p for ps in project.get("optional-dependencies", {}).values() for p in ps]
+    if "name" in project:
+        pkgs.append(project["name"])
+    interesting_packages = {Requirement(p).name for p in pkgs}
+    interesting_packages.add("pip")
+
+    valid = []
+    for package in sorted(interesting_packages):
+        with contextlib.suppress(ModuleNotFoundError):
+            valid.append(f"{package}=={metadata.version(package)}")
+    reqs = " ".join(valid)
+    lines = [
+        f"installed packages of interest: {reqs}",
+        f"sysconfig platform: {sysconfig.get_platform()}",
+    ]
+    return "\n".join(lines)
---
+++ tests/test_pyproject_pep660.py
@@ -0,0 +1,118 @@
+import sys
+import sysconfig
+import typing
+import zipfile
+from pathlib import Path
+
+import pytest
+
+from scikit_build_core_conan.build import build_editable
+
+
+@pytest.fixture(params=["redirect", "inplace"])
+def editable_mode(request: pytest.FixtureRequest) -> str:
+    return typing.cast("str", request.param)
+
+
+# TODO: figure out why gmake is reporting no rule to make simple_pure.cpp
+@pytest.mark.compile
+@pytest.mark.configure
+@pytest.mark.xfail(
+    sys.platform.startswith("cygwin"),
+    strict=False,
+    reason="No idea why this fails on Cygwin",
+)
+@pytest.mark.usefixtures("package_simplest_c")
+def test_pep660_wheel(editable_mode: str, tmp_path: Path):
+    dist = tmp_path / "dist"
+    out = build_editable(str(dist), {"editable.mode": editable_mode})
+    (wheel,) = dist.glob("simplest-0.0.1-*.whl")
+    assert wheel == dist / out
+
+    with zipfile.ZipFile(wheel) as zf:
+        file_names = {Path(p).parts[0] for p in zf.namelist()}
+
+        with zf.open("simplest-0.0.1.dist-info/METADATA") as f:
+            metadata = f.read().decode("utf-8")
+
+    assert len(file_names) == 4 if editable_mode == "redirect" else 2
+    assert "simplest-0.0.1.dist-info" in file_names
+    if editable_mode == "redirect":
+        assert "simplest" in file_names
+        assert "_simplest_editable.py" in file_names
+    else:
+        assert "simplest" not in file_names
+        assert "_simplest_editable.py" not in file_names
+    assert "_simplest_editable.pth" in file_names
+
+    assert "Metadata-Version: 2.2" in metadata
+    assert "Name: simplest" in metadata
+    assert "Version: 0.0.1" in metadata
+
+
+@pytest.mark.compile
+@pytest.mark.configure
+@pytest.mark.integration
+@pytest.mark.parametrize("isolate", [True, False], ids=["isolated", "not_isolated"])
+@pytest.mark.usefixtures("package_simplest_c")
+def test_pep660_pip_isolated(isolated, isolate, editable_mode: str):
+    isolate_args = ["--no-build-isolation"] if not isolate else []
+    isolated.install("pip>=23")
+    if not isolate:
+        isolated.install("scikit-build-core-conan")
+
+    build_dir = "" if editable_mode == "inplace" else "build/{wheel_tag}"
+
+    isolated.install(
+        "-v",
+        f"--config-settings=build-dir={build_dir}",
+        f"--config-settings=editable.mode={editable_mode}",
+        *isolate_args,
+        "-e",
+        ".",
+    )
+
+    value = isolated.execute("import simplest; print(simplest.square(2))")
+    assert value == "4.0"
+
+    location_str = isolated.execute(
+        "import simplest; print(*simplest.__path__, sep=';')"
+    )
+    locations = [Path(s).resolve() for s in location_str.split(";")]
+
+    # First path is from the python source
+    python_source = Path("src/simplest").resolve()
+    assert any(x.samefile(python_source) for x in locations)
+
+    cmake_install = isolated.platlib.joinpath("simplest").resolve()
+    if editable_mode == "redirect":
+        # Second path is from the CMake install
+        assert any(x.samefile(cmake_install) for x in locations)
+
+    location = isolated.execute("import simplest; print(simplest.__file__)")
+    # The package file is defined in the python source and __file__ must point to it
+    assert Path("src/simplest/__init__.py").resolve().samefile(Path(location).resolve())
+
+    location = isolated.execute(
+        "import simplest._module; print(simplest._module.__file__)"
+    )
+
+    if sys.version_info < (3, 8, 7):
+        import distutils.sysconfig  # pylint: disable=deprecated-module
+
+        ext_suffix = distutils.sysconfig.get_config_var("EXT_SUFFIX")
+    else:
+        ext_suffix = sysconfig.get_config_var("EXT_SUFFIX")
+
+    module_source = python_source if editable_mode == "inplace" else cmake_install
+    module_file = module_source / f"_module{ext_suffix}"
+
+    # Windows FindPython may produce the wrong extension
+    if (
+            sys.version_info < (3, 8, 7)
+            and sys.platform.startswith("win")
+            and not module_file.is_file()
+    ):
+        module_file = module_source / "_module.pyd"
+
+    assert module_file.samefile(Path(location).resolve())
