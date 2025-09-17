2c48ab35cc4c1a89d11111e7135933eae9e16e17
Vincent <magicdroidx@gmail.com>
2025-09-17 23:19:40 +0100

    refactor: expand ConanSettings fields

--- src/scikit_build_core_conan/build/settings.py
+++ src/scikit_build_core_conan/build/settings.py
@@ -1,5 +1,4 @@
 import dataclasses
-from typing import List
 
 
 @dataclasses.dataclass
@@ -11,14 +10,27 @@ class ConanLocalRecipesSettings:
 
 @dataclasses.dataclass
 class ConanSettings:
+    # common
     path: str = "."
+    # common install
     build: str = "missing"
+    remote: list[str] | None = None
+    no_remote: bool = False
+    # profile
     profile: str = ""
-    options: List[str] = dataclasses.field(default_factory=list)
-    settings: List[str] = dataclasses.field(default_factory=list)
-    config: List[str] = dataclasses.field(default_factory=list)
+    profile_build: str = ""
+    profile_all: str = ""
+    options: list[str] = dataclasses.field(default_factory=list)
+    options_build: list[str] = dataclasses.field(default_factory=list)
+    options_all: list[str] = dataclasses.field(default_factory=list)
+    settings: list[str] = dataclasses.field(default_factory=list)
+    settings_build: list[str] = dataclasses.field(default_factory=list)
+    settings_all: list[str] = dataclasses.field(default_factory=list)
+    config: list[str] = dataclasses.field(default_factory=list)
+    config_build: list[str] = dataclasses.field(default_factory=list)
+    config_all: list[str] = dataclasses.field(default_factory=list)
+    # install
     generator: str = ""
     output_folder: str = ""
-    local_recipes: List[ConanLocalRecipesSettings] = dataclasses.field(
-        default_factory=list
-    )
+    # extra
+    local_recipes: list[ConanLocalRecipesSettings] = dataclasses.field(default_factory=list)
